75d5a1da8de33fa8d4eedb9375b5272fb1217798
ademonteverde <108604045+ademonteverde@users.noreply.github.com>
2023-07-25 20:57:44 -0400

    updated user.js user.Routs.js. connection.js index.js spi/index.js

---
+++ models/User.js
@@ -0,0 +1,60 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class User extends Model {
+  async checkPassword (userPassword) {
+    console.log("userPassword", userPassword);
+    console.log("password stored", this.password);
+    const val = await bcrypt.compare(userPassword, this.password);
+    return val;
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8]
+      }
+    }
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user"
+  }
+);
+
+module.exports = User;
