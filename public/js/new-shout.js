f5ff27f57e03e39631e835605d645ef010d8b594
hannahrajarao <hannahrajarao@gmail.com>
2023-08-03 19:53:43 -0400

    Lint

--- public/js/new-shout.js
+++ public/js/new-shout.js
@@ -1,4 +1,4 @@
-function type(elem) {
-    elem.value = elem.value.toUpperCase();
-    document.querySelector('#character-count').innerHTML = elem.value.length;
-}
+// function type(elem) {
+//   elem.value = elem.value.toUpperCase();
+//   document.querySelector("#character-count").innerHTML = elem.value.length;
+// }
