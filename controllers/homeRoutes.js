97e64c43a85dfc5be53b7c934aa649583c0feeca
hannahrajarao <hannahrajarao@gmail.com>
2023-08-03 19:55:14 -0400

    Lint

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const withAuth = require("../utils/auth");
+// const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
