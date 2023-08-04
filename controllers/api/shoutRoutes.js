5e1f0ed17340694db2b199a2b35d1734f4b2c72a
Joaquin Florencio <Jrr232@gmail.com>
2023-08-03 21:05:39 -0400

    first commit

--- controllers/api/shoutRoutes.js
+++ controllers/api/shoutRoutes.js
@@ -1,40 +1,54 @@
 const router = require("express").Router();
-const Shout = require("../../models/Shout");
+const { Shout, User } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 // route to create/add a shout using async/await
-router.post("/", withAuth, async (req, res) => {
-  try {
-    const shoutData = await Shout.create({
-      user_id: req.body.user_id,
-      text: req.body.text
+router.post("/", async (req, res) => {
+    try {
+        const shoutData = await Shout.create({
+            user_id: req.body.user_id,
+            text: req.body.text
 
-    });
+        });
 
-    res.status(200).json(shoutData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+        res.status(200).json(shoutData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
-router.get("/:user_id", withAuth, async (req, res) => {
-  try {
-    const shoutData = await Shout.findAll({
-      where: {
-        user_id: req.params.user_id
-      }
-    });
+router.get("/:user_id", async (req, res) => {
+    try {
+        const shoutData = await Shout.findAll({
+            include: [
+                {
+                    model: User,
 
-    const shouter = shoutData.map((shout) => shout.get({ plain: true }));
+                },
+            ],
+            where: {
+                user_id: req.params.user_id
+            }
+        });
+        const userData = await User.findAll({
+            where: {
+                id: req.params.user_id
+            }
+        });
 
-    console.log(shouter);
-    res.render("./partials/shout-details", {
-      shouter
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+        const shouter = shoutData.map((shout) => shout.get({ plain: true }));
+        const username = userData.map((user) => user.get({ plain: true }));
+        console.log(shouter);
+        console.log(username);
+
+        res.render("./partials/shout-details", {
+            shouter,
+            username
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
