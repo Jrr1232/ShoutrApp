88bdced427d8363b51711c0c61b6ccc01651c622
Joaquin Florencio <Jrr232@gmail.com>
2023-08-05 17:43:11 -0400

    rendering shouts

--- controllers/api/shoutRoutes.js
+++ controllers/api/shoutRoutes.js
@@ -1,55 +1,54 @@
 const router = require("express").Router();
 const { Shout, User } = require("../../models");
-// const withAuth = require("../../utils/auth");
+const withAuth = require("../../utils/auth");
 
 // route to create/add a shout using async/await
-router.post("/", async (req, res) => {
-  try {
-    const shoutData = await Shout.create({
-      user_id: req.body.user_id,
-      text: req.body.text
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const shoutData = await Shout.create({
+            user_id: req.body.user_id,
+            text: req.body.text
 
-    });
+        });
 
-    res.status(200).json(shoutData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+        res.status(200).json(shoutData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
-router.get("/:user_id", async (req, res) => {
-  try {
-    const shoutData = await Shout.findAll({
-      include: [
-        {
-          model: User
+router.get("/:user_id", withAuth, async (req, res) => {
+    try {
+        const shoutData = await Shout.findAll({
+            include: [
+                {
+                    model: User
 
-        }
-      ],
-      where: {
-        id: req.params.user_id
-      }
-    });
-    const userData = await User.findAll({
-      where: {
-        id: req.params.user_id,
+                }
+            ],
+            where: {
+                user_id: req.params.user_id
+            }
+        });
+        const userData = await User.findAll({
+            where: {
+                id: req.params.user_id,
 
-      }
-    });
+            }
+        });
 
-    const shouter = shoutData.map((shout) => shout.get({ plain: true }));
-    const username = userData.map((user) => user.get({ plain: true }));
-    console.log(shouter);
-    console.log(username);
+        const shouter = shoutData.map((shout) => shout.get({ plain: true }));
+        const username = userData.map((user) => user.get({ plain: true }));
+        console.log(shouter);
 
-    res.render("./partials/shout-details", {
-      shouter,
-      username
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+        res.render("./partials/shout-details", {
+            shouter,
+            username
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -1,17 +1,17 @@
 const sequelize = require("../config/connection");
-const seedShout = require("../shoutData");
-// const { User } = require("../models");
+const { User, Shout } = require("../models");
+const seedShout = require("./shoutData.json");
+const userData = require("./userData.json");
 
-// const userData = require("./userData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await seedShout.sync();
-  // await User.bulkCreate(userData, {
-  //   individualHooks: true,
-  //   returning: true
-  // });
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+  });
+  await Shout.bulkCreate(seedShout)
 
   process.exit(0);
 };
