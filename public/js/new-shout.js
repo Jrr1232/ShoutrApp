081f6cf6c5ec67ec1cc1447547a09ae87063c840
ademonteverde <108604045+ademonteverde@users.noreply.github.com>
2023-08-07 19:16:57 -0400

    updated signup

--- public/js/new-shout.js
+++ public/js/new-shout.js
@@ -2,7 +2,7 @@ function inputCharacter (elem) { // eslint-disable-line no-unused-vars
   elem.value = elem.value.toUpperCase();
   const characterCount = elem.value.length;
   document.querySelector("#character-count").innerHTML = characterCount;
-};
+}
 
 const postNewShout = async (event) => {
   event.preventDefault();
