3755138849bb60390eead2c555a2d66b48bd0b0a
hannahrajarao <hannahrajarao@gmail.com>
2023-08-07 08:10:23 -0400

    Function handle creating new shout

--- public/js/new-shout.js
+++ public/js/new-shout.js
@@ -3,3 +3,12 @@ function inputCharacter (elem) { // eslint-disable-line no-unused-vars
   const characterCount = elem.value.length;
   document.querySelector("#character-count").innerHTML = characterCount;
 }
+
+async function createNewShout() { // eslint-disable-line no-unused-vars
+  const shoutText = document.querySelector("#new-shout").value;
+  const response = await fetch("/api/shout", {
+    method: "POST",
+    body: JSON.stringify({ user_id, shoutText }),
+    headers: { "Content-Type": "application/json" }
+  });
+}
