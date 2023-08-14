49f4a28088b13c0287bcee0587b6cfeed138b30b
Joaquin Florencio <Jrr232@gmail.com>
2023-08-14 18:37:50 -0400

    addingJawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-let sequelize = ""
+let sequelize = "";
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     " mysql://dclmn62ol4j95g2m:s0v8gevgdz4g9p6q@rwo5jst0d7dgy0ri.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vw37u7v3v7liwmz1");
